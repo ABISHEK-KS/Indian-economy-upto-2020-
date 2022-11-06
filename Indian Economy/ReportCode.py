9e48b0721a405068caa4a3e03bcccd330cec6c80
ABISHEK-KS <97246536+ABISHEK-KS@users.noreply.github.com>
2022-11-06 18:00:46 +0530

    Delete ReportCode.py

--- Indian Economy/ReportCode.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd 
-import pandas_profiling as pp 
-import csv 
-filin=pd.read_csv("indianEco.csv")
-
-profrep=pp.ProfileReport(filin)
-profrep.to_file("ProfilingReport.html")
